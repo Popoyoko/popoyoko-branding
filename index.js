14fc520935f2309b0d8f5d4256e92d6cf32b6ecc
LouiFi <louis@popoyoko.fr>
2023-02-18 20:33:07 +0100

    ⚗️ Trying to use as binary

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const getColors = () => {
 };
 
 const exportSassColors = () => {
-  console.log("\x1b[33m\x1b[1m%s\x1b[0m", "colors.scss generated");
   let sass = "";
   for (const colorName in colors) {
     sass += `$${colorName}: ${colors[colorName]};\n`;
   }
   fs.writeFileSync("colors.scss", sass);
+  console.log("\x1b[33m\x1b[1m%s\x1b[0m", "colors.scss generated");
 };
 
 module.exports = {
